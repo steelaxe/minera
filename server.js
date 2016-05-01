a6fc876cf109c9161b289f8f1ae2faa9b78c5e59
TakenoriOkada <okada@okada-no-MacBook-Pro.local>
2016-05-01 10:38:26 +0900

    qavppyのファイルでうわがき

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+// ************************************************
 
 // Get secrets from server environment
 var botConnectorOptions = {
     appId: process.env.BOTFRAMEWORK_APPID,
     appSecret: process.env.BOTFRAMEWORK_APPSECRET
 };
+
+// ************************************************
+
+// MongoLabの設定
+var options = {
+    server: {
+        socketOptions: {
+            keepAlive: 300000,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 300000,
+            connectTimeoutMS: 30000
+        }
+    }
+};
+var mongodbUri = 'mongodb://okajax:avmomo92@ds032319.mlab.com:32319/my-bot-db';
+
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+//支出額のスキーマを宣言
+var OutgoSchema = new mongoose.Schema({
+    date: { type: Date, default: Date.now },
+    amount: Number,
+    category: String
+});
+//スキーマからモデルを生成。
+mongoose.model('Outgo', OutgoSchema);
+
+// ************************************************
 
 // Dialog
 var dialog = new builder.CommandDialog();
